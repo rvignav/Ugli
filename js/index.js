42056d659182ca6aa42f1335ca05aaf88ec68db9
masonwang025 <mason.wang0025@gmail.com>
2020-04-11 16:29:15 -0700

    filtering works

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+// render elements to DOM
+var renderToDOM = (name, location, ingredients) => {};
+
 // get all restaurant cards and apply column classes
 $(".restaurant-card").addClass(
   "col col-sm-6 col-md-4 col-lg-3 shadow card card-body"
@@ -21,27 +24,42 @@ var $quicksearch = $("#search-ingredient").keyup(
     qsRegex = new RegExp($quicksearch.val(), "gi");
     $grid.isotope({
       filter: function () {
-        return qsRegex
-          ? $(this).find(".ingredients").text().match(qsRegex)
-          : true;
+        if (qsRegex) {
+            if (qsRegex2) {
+              return (
+                $(this).find(".location").text().match(qsRegex2) &&
+                $(this).find(".ingredients").text().match(qsRegex)
+              );
+            } else {
+              return $(this).find(".ingredients").text().match(qsRegex);
+            }
+          } else return true;
       },
     });
   }, 200)
 );
 
+var qsRegex2;
 // use value of location field to filter
 var $quicksearch2 = $("#search-location").keyup(
-    debounce(function () {
-      qsRegex = new RegExp($quicksearch2.val(), "gi");
-      $grid.isotope({
-        filter: function () {
-          return qsRegex
-            ? $(this).find(".location").text().match(qsRegex)
-            : true;
-        },
-      });
-    }, 200)
-  );
+  debounce(function () {
+    qsRegex2 = new RegExp($quicksearch2.val(), "gi");
+    $grid.isotope({
+      filter: function () {
+        if (qsRegex2) {
+          if (qsRegex) {
+            return (
+              $(this).find(".location").text().match(qsRegex2) &&
+              $(this).find(".ingredients").text().match(qsRegex)
+            );
+          } else {
+            return $(this).find(".location").text().match(qsRegex2);
+          }
+        } else return true;
+      },
+    });
+  }, 200)
+);
 
 // debounce so filtering doesn't happen every millisecond
 function debounce(fn, threshold) {
