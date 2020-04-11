5c25802222aaed9024a5ceca7abc94d317752413
masonwang025 <mason.wang0025@gmail.com>
2020-04-11 16:03:11 -0700

    isotope filter

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // get all restaurant cards and apply column classes
-$(".restaurant-card").addClass("col col-sm-6 col-md-4 col-lg-3 shadow");
+$(".restaurant-card").addClass(
+  "col col-sm-6 col-md-4 col-lg-3 shadow card card-body"
+);
 
 // quick search regex
 var qsRegex;
@@ -9,17 +11,37 @@ var $grid = $(".grid").isotope({
   itemSelector: ".restaurant-card",
   layoutMode: "fitRows",
   filter: function () {
-    return qsRegex ? $(this).find('.name').text().match(qsRegex) : true;
+    return qsRegex ? $(this).find(".ingredients").text().match(qsRegex) : true;
   },
 });
 
-// use value of search field to filter
+// use value of ingredient field to filter
 var $quicksearch = $("#search-ingredient").keyup(
   debounce(function () {
     qsRegex = new RegExp($quicksearch.val(), "gi");
-    $grid.isotope();
+    $grid.isotope({
+      filter: function () {
+        return qsRegex
+          ? $(this).find(".ingredients").text().match(qsRegex)
+          : true;
+      },
+    });
   }, 200)
 );
+
+// use value of location field to filter
+var $quicksearch2 = $("#search-location").keyup(
+    debounce(function () {
+      qsRegex = new RegExp($quicksearch2.val(), "gi");
+      $grid.isotope({
+        filter: function () {
+          return qsRegex
+            ? $(this).find(".location").text().match(qsRegex)
+            : true;
+        },
+      });
+    }, 200)
+  );
 
 // debounce so filtering doesn't happen every millisecond
 function debounce(fn, threshold) {
