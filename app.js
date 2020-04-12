de896c5f57ab1187eaf39317a6a03f0310f391b8
Howard Huang <howardhuang2004@gmail.com>
2020-04-11 18:13:41 -0700

    delete restaurant function enabled

--- app.js
+++ app.js
@@ -6,19 +6,29 @@ function renderStore(doc){
   let li = document.createElement('li');
   let name = document.createElement('span');
   let city = document.createElement('span');
+  let cross = document.createElement('div');
 
   li.setAttribute('data-id', doc.id);
   name.textContent = doc.data().name;
   city.textContent = doc.data().city;
+  cross.textContent = 'x';
 
   li.appendChild(name);
   li.appendChild(city);
+  li.appendChild(cross);
 
   storeList.appendChild(li);
 
   console.log(doc.data().name);
   console.log(doc.data().city);
   console.log(doc.data().ingredients);
+
+  // deleting data
+  cross.addEventListener('click', (e) => {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute('data-id')
+    db.collection('Restaurants').doc(id).delete();
+  })
 }
 
 // getting data
@@ -35,4 +45,6 @@ form.addEventListener('submit', (e) => {
     name: form.name.value,
     city: form.city.value
   });
+  form.name.value = '';
+  form.city.value = '';
 })
